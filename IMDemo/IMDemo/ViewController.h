81c6511008c7ab7462775c72413f013e247b449f
summer-mm <iMac@iMacdeiMac-2.local>
2016-05-26 15:18:32 +0800

    IMdemo

---
+++ IMDemo/IMDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IMDemo
+//
+//  Created by iMac on 16/5/26.
+//  Copyright © 2016年 carlos_mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IMDemo/IMDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IMDemo
+//
+//  Created by iMac on 16/5/26.
+//  Copyright © 2016年 carlos_mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
